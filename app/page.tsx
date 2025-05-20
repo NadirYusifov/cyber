ba0ddb018a1ee26bd84d4dd1f1da6197e4c8b9d1
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-05-21 02:05:22 +0400

    Home page commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import IPhoneImage from "../public/iphoneimage.png";
 
 export default function Home() {
   return (
+    // <section className="bg-linear h-screen">
+    //   <div className="container mx-auto px-4 block lg:flex lg:justify-between">
+    //     <div className="hero-content">
+    //       <article className="hero-text">
+    //         <h4>Pro.Beyond</h4>
+    //         <h1>IPhone 14 Pro</h1>
+    //         <p>Created to change everything for the better. For everyone</p>
+    //       </article>
+    //       <div className="hero-button">
+    //         <button className="bg-transparent border-1 border-white text-white rounded-lg px-8 py-3">
+    //           Shop Now
+    //         </button>
+    //       </div>
+    //     </div>
+    //     <div>
+    //       <Image src={IPhoneImage} width={417} height={417} quality={100} alt="Iphone Image"/>
+    //     </div>
+    //   </div>
+    // </section>
     <>
-    
     </>
   );
 }
