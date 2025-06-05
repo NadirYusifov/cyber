fadd8920b49e7546dd31ac14884a51abd0d4a785
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-06 01:11:24 +0400

    3 pieces add section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import BannersSecondSection from "@/pages/bannerssecondsection";
 import BannersSection from "@/pages/bannerssection";
+import DiscountsProducts from "@/pages/discountsproducts";
 import CategoriesSection from "@/pages/homepage/categoriessction";
 import ElectronicSection from "@/pages/homepage/electronicsection";
 import HeroSection from "@/pages/homepage/herosection";
+import ProductsSection from "@/pages/productssection";
 
 export default function Home() {
   return (
@@ -9,7 +12,10 @@ export default function Home() {
       <HeroSection />
       <ElectronicSection />
       <CategoriesSection />
+      <ProductsSection />
       <BannersSection />
+      <DiscountsProducts />
+      <BannersSecondSection />
     </main>
   );
 }
