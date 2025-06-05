01cd40ae32fe1ab0e3ba54662905cf62ffeafafd
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-06 01:13:18 +0400

    Changed px to rem in the Header Link

--- components/layout/Header/HeaderLink/index.tsx
+++ components/layout/Header/HeaderLink/index.tsx
@@ -11,7 +11,7 @@ export default function HeaderLink() {
                 </div>
                 <input type='search' className='w-[360px] outline-none px-10 bg-cultured rounded-lg py-3' placeholder='Search' />
             </div>
-            <div className='text-[30px] text-american-silver lg:text-[16px] flex items-center flex-col lg:flex-row gap-y-3 lg:gap-13 transition-all'>
+            <div className='text-[1.875rem] text-american-silver lg:text-[1rem] flex items-center flex-col lg:flex-row gap-y-3 lg:gap-13 transition-all'>
                 <Link className='hover:text-black' href={"/"}>Home</Link>
                 <Link className='hover:text-black' href={"#"}>About</Link>
                 <Link className='hover:text-black' href={"#"}>Contact Us</Link>
