27fff24227306b46601b11830c35fd4a29030411
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-09 01:21:28 +0400

    Refactor CategoriesSection to use dynamic slider items and improve responsive settings but not ready

--- pages/homepage/categoriessction/index.tsx
+++ pages/homepage/categoriessction/index.tsx
@@ -4,8 +4,53 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { NextArrow, PrevArrow } from '@/components/common/arrow';
+import { Rows } from 'lucide-react';
 
 export default function CategoriesSection() {
+
+    const slideritem = [
+        {
+            icon: '📱',
+            title: 'Phones'
+        },
+        {
+            icon: '⌚',
+            title: 'Smart Watches'
+        },
+        {
+            icon: '📷',
+            title: 'Cameras'
+        },
+        {
+            icon: '🎧',
+            title: 'Headphones'
+        },
+        {
+            icon: '🖥️',
+            title: 'Computers'
+        },
+        {
+            icon: '🎮',
+            title: 'Gaming'
+        },
+        {
+            icon: '',
+            title: 'Tablets'
+        },
+        {
+            icon: '💻',
+            title: 'Laptops'
+        },
+        {
+            icon: '🕶️',
+            title: 'VR'
+        },
+        {
+            icon: '⌨️',
+            title: 'Accessory'
+        }
+    ]
+
     const settings = {
         dots: false,
         infinite: true,
@@ -18,11 +63,23 @@ export default function CategoriesSection() {
             {
                 breakpoint: 600,
                 settings: {
+                    centerMode: true,
                     slidesToScroll: 1,
-                    slidesToShow: 2,
-                    slidesPerRow: 3
+                    slidesToShow: 1,
+                    slidesPerRow: 2,
+                    rows: 3
                 }
-            }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    centerMode: true,
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                    slidesPerRow: 2,
+                    rows: 3
+                }
+            },
         ]
     }
 
@@ -30,51 +87,19 @@ export default function CategoriesSection() {
         <section className='categories-section my-32'>
             <div className='container mx-auto px-4'>
                 <div className='categories-area'>
-                    <article className='categories-header inline-block mb-8'>
-                        <h3 className='text-2xl tracking-[1%] leading-8 font-medium'>Browse By Category</h3>
+                    <article className='categories-header mb-8 inline-block relative'>
+                        <h3 className='text-2xl tracking-[1%] leading-8 font-medium inline-block'>Browse By Category</h3>
                     </article>
                     <div className='categories-slider text-center'>
                         <Slider {...settings}>
-                            <div className='bg-bright-gray w-40 h-[7.5rem] !flex flex-col justify-center'>
-                                <span>📱</span>
-                                <article>Phones</article>
-                            </div>
-                            <div className='bg-bright-gray'>
-                                <span>⌚</span>
-                                <article>Smart Watches</article>
-                            </div>
-                            <div>
-                                <span>📷</span>
-                                <article>Cameras</article>
-                            </div>
-                            <div>
-                                <span>🎧</span>
-                                <article>Headphones</article>
-                            </div>
-                            <div>
-                                <span>🖥️</span>
-                                <article>Computers</article>
-                            </div>
-                            <div>
-                                <span>🎮</span>
-                                <article>Gaming</article>
-                            </div>
-                            <div>
-                                <span></span>
-                                <article>Tablets</article>
-                            </div>
-                            <div>
-                                <span>💻</span>
-                                <article>Laptops</article>
-                            </div>
-                            <div>
-                                <span>🕶️</span>
-                                <article>VR</article>
-                            </div>
-                            <div>
-                                <span>⌨️</span>
-                                <article>Accessory</article>
-                            </div>
+                            {slideritem.map((item, index) => (
+                                <div className='flex flex-row justify-center items-center px-2 py-3'>
+                                    <div className='bg-bright-gray rounded-[0.938rem] w-full  h-[7.5rem] !flex flex-col justify-center' key={index}>
+                                        <span>{item.icon}</span>
+                                        <article className='text-[1rem] font-medium leading-6'>{item.title}</article>
+                                    </div>
+                                </div>
+                            ))}
                         </Slider>
                     </div>
                 </div>
