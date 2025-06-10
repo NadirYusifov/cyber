99c9717771f3fb0f7c20e28a9fd1897310d9bda9
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-11 02:26:40 +0400

    Banner Section Image added but content not ready yet

--- pages/bannerssecondsection/index.tsx
+++ pages/bannerssecondsection/index.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
+import BannerSecondImage from '../../public/bannersecond.png';
+
 export default function BannersSecondSection() {
   return (
-    <div>BannersSecondSection</div>
+    <section className='w-full mt-20'>
+      <div className='relative'>
+        <picture className='w-full relative h-64'>
+          <Image src={BannerSecondImage} width={6000} height={448} quality={100} alt='Banner 2 Image'/>
+        </picture>
+        <article className='absolute top-1/2  text-white'>
+          <h3>Big Summer Sale</h3>
+          <p>Test content</p>
+        </article>
+      </div>
+    </section>
   )
 }
