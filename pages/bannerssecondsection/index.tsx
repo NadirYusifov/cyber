db9fe1e5404d9bd47c82b7e0380496eb2170414a
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-12 01:16:21 +0400

    BannerSecondSection content write

--- pages/bannerssecondsection/index.tsx
+++ pages/bannerssecondsection/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import BannerSecondImage from '../../public/bannersecond.png';
+import { Button } from '@/components/ui/button';
 
 export default function BannersSecondSection() {
   return (
@@ -8,9 +9,10 @@ export default function BannersSecondSection() {
         <picture className='w-full relative h-64'>
           <Image src={BannerSecondImage} width={6000} height={448} quality={100} alt='Banner 2 Image'/>
         </picture>
-        <article className='absolute top-1/2  text-white'>
-          <h3>Big Summer Sale</h3>
-          <p>Test content</p>
+        <article className='absolute top-2/5 bottom-0 translate-x-0 left-0 right-0 text-center'>
+          <h3 className='text-white text-[4.5rem] font-thin leading-[4.5rem]'>Big Summer <span className='font-medium'>Sale</span></h3>
+          <p className='text-sonic-silver text-[1rem] leading-8 pb-10'>Commodo fames vitae vitae leo mauris in. Eu consequat.</p>
+          <Button variant={"outline"} size={"lg"}>Shop Now</Button>
         </article>
       </div>
     </section>
