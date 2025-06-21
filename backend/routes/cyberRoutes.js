ad3e59c17a27e4d52920e59e11db9e906214bc68
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-22 02:33:16 +0400

    cyberRoutes some method added

--- backend/routes/cyberRoutes.js
+++ backend/routes/cyberRoutes.js
@@ -1,3 +1,12 @@
 import { Router } from 'express';
-import { productGet } from '../controllers/productGet';
+import { productGet } from '../controllers/productGet.js';
+import { productPost } from '../controllers/productPost.js';
+import { productGetId } from '../controllers/productGetId.js';
 
+export const cyberProductsRouter = Router();
+
+cyberProductsRouter.get('/', productGet);
+cyberProductsRouter.get('/:id', productGetId);
+
+// POST
+cyberProductsRouter.post('/', productPost);
