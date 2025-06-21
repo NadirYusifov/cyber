497e6faa4a448a2d51448460c2de07082ce802d0
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-22 02:31:51 +0400

    app.use includes cyberProductsRouter

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import { cyberProductsRouter } from './routes/cyberRoutes.js';
 
 const app = express();
 const PORT = 7575;
@@ -7,9 +8,7 @@ const PORT = 7575;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use('/products', cyberProductsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
