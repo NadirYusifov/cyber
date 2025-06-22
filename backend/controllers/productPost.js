0ded59866ff6f4e93c968c5a6aa160d49cc8b877
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-23 02:14:32 +0400

    mongoose with data created but not completed

--- backend/controllers/productPost.js
+++ backend/controllers/productPost.js
@@ -1,8 +1,23 @@
+import { cyberProductModels } from "../models/cyberProductModels.js"
+
 export const productPost = (req, res) => {
     const data = req.body
-    res.status(201).send({
-        status: 201,
-        message: 'Product created successfully',
-        data: data
-    })
-}
+    cyberProductModels.create(data).then((result) => {
+        try {
+            if (data && data.name && data.description && data.price) {
+                res.status(201).send({
+                    status: 201,
+                    message: 'Product created successfully',
+                    data: result
+                })
+            } else {
+                res.status(400).send({
+                    status: 400,
+                    message: 'This have product',
+                });
+            };
+        } catch (error) {
+            console.log('Error creating product', error.message);
+        };
+    });
+};
