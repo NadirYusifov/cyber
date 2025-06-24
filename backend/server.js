fd6d0fd77f0282f9aa6b831e6428a9f463f35d77
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-25 01:21:37 +0400

    ; sign added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import { cyberProductsRouter } from './routes/cyberRoutes.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { cyberProductsRouter } from "./routes/cyberRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,20 +11,19 @@ dotenv.config();
 
 const PORT = 7575;
 
-app.use('/products', cyberProductsRouter);
+app.use("/products", cyberProductsRouter);
 
 const mongoconnect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI).then(() => {
-        console.log('Connected to MongoDB');
-      })
-    } catch (error) {
-        console.log('Error connecting to MongoDB', error.message);
-    };
+  try {
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
+      console.log("Connected to MongoDB");
+    });
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 console.log(mongoconnect());
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
