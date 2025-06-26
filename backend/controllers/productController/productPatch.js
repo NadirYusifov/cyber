28c24e8f3f36e16a3ecd4c9fd678e049a35fd6cc
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-27 01:14:25 +0400

    update productPath

--- backend/controllers/productController/productPatch.js
+++ backend/controllers/productController/productPatch.js
@@ -1,23 +1,23 @@
-import { cyberProductModels } from "../../models/cyberProductModels.js";
+import { cyberProductModels } from "../../models/cyberproductModels.js";
 
 export const productPatch = (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   if (id) {
     try {
       cyberProductModels.findByIdAndUpdate(id, req.body).then(() => {
-        res.status(200).send({
+        res.status(200).json({
           status: 200,
           message: "Update sucessfully",
         });
       });
     } catch {
-      res.status(500).send({
+      res.status(500).json({
         status: 500,
         message: "Problem",
       });
     }
   } else {
-    res.status(400).send({
+    res.status(400).json({
       status: 400,
       message: "Bad request",
     });
