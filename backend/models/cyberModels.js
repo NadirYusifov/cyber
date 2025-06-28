ed620914e7c2becadd5b74a9917c5a1e2e7904ef
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-29 02:11:43 +0400

    cyberModels added

---
+++ backend/models/cyberModels.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { cyberauthSchema } from "../schemas/cyberSchema.js";
+import { cyberproductsSchema } from "../schemas/cyberSchema.js";
+
+export const cyberauthModels = mongoose.model("auth", cyberauthSchema);
+export const cyberproductsModels = mongoose.model(
+  "products",
+  cyberproductsSchema,
+);
