06622fa256f5c3e8f5ad963dbee57d083d0b8281
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-29 02:12:54 +0400

    adminloginGet, adminloginPost added cyberRoutes file

--- backend/routes/cyberauthRoutes.js
+++ backend/routes/cyberauthRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { adminloginGet } from "../controllers/authController/adminloginGet.js";
+import { adminloginPost } from "../controllers/authController/adminloginPost.js";
 
-export const cyberauthRoutes = Router;
+export const cyberauthRoutes = Router();
 
 // GET
-cyberauthRoutes.get("/", loginGet);
+cyberauthRoutes.get("/", adminloginGet);
+
+// POST
+cyberauthRoutes.post("/", adminloginPost);
