5db2234be2fa64dc1ad73fd006aa91117526cc4e
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-06-29 02:11:54 +0400

    delete

--- backend/models/cyberProductModels.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import { cyberProductSchema } from "../schemas/cyberProductSchema.js";
-
-export const cyberProductModels = mongoose.model('products', cyberProductSchema);
