e9f7e9e1784dbc8697c35491dd59d1fef7e3e8c3
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-06 20:25:39 +0400

    admin data of added

--- backend/controllers/authController/adminloginPost.js
+++ backend/controllers/authController/adminloginPost.js
@@ -1,10 +1,48 @@
-export const adminloginPost = (req, res) => {
-  try {
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: "Server Error",
-      error: error.message,
-    });
-  }
+import bcrypt from "bcrypt";
+import { cyberauthModels } from "../../models/cyberModels.js"
+
+export const adminloginPost = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Admin data username findOne
+        const isUsername = await cyberauthModels.findOne({ username: "admin" });
+        const isPassword = await cyberauthModels.findOne({ password: "admin123" })
+        if (isUsername) {
+            res.status(400).json({
+                status: 400,
+                message: "Username already exists",
+                success: false
+            })
+            return;
+        };
+
+        if (isPassword) {
+            res.status(400).json({
+                status: 400,
+                message: "Password already exists",
+                success: false
+            })
+            return;
+        };
+
+        // Admin data username create
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await cyberauthModels.create({ username, password: hashedPassword }).then(async () => {
+            res.status(201).json({
+                status: 201,
+                message: "Login Created",
+                success: true
+            });
+            return;
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            status: 500,
+            message: "Server Error",
+            success: false,
+            error: error.message
+        });
+    };
 };
