5d58220fa5e2872dd2d843dfb7e78819bf669cda
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-06 20:25:09 +0400

    login on jwt, bcrypt, dotenv with admin data of checked

--- backend/controllers/authController/adminloginGet.js
+++ backend/controllers/authController/adminloginGet.js
@@ -1,14 +1,39 @@
-export const adminloginGet = (req, res) => {
+import bcyrpt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { cyberauthModels } from "../../models/cyberModels.js";
+
+export const adminloginGet = async (req, res) => {
+  dotenv.config();
+
+  const { username, password } = req.body;
   try {
-    res.status(200).json({
-      status: 200,
-      message: "OK",
-    });
+    const isUsername = await cyberauthModels.findOne({ username: "admin" });
+
+    if (isUsername) {
+      bcyrpt.compare(password, isUsername.password, (error, result) => {
+        if (result) {
+          const token = jwt.sign({ username: isUsername.username }, process.env.JWT_SECURITY_KEY, { expiresIn: "1h" })
+          res.status(200).json({
+            status: 200,
+            message: "Login successfully",
+            success: true,
+            token: token
+          })
+        } else {
+          res.status(401).json({
+            status: 401,
+            message: "Unauth",
+            success: false
+          });
+        };
+      });
+    };
   } catch (error) {
     res.status(500).json({
       status: 500,
       message: "error",
       error: error.message,
     });
-  }
+  };
 };
