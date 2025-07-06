e18abdb6fa8fdb67a43149e1652ea17bccff4b31
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-07 00:28:56 +0400

    ../../../ remove and change @

--- frontend/pages/homepage/herosection/index.tsx
+++ frontend/pages/homepage/herosection/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
-import IPhoneImage from '../../../public/iphoneimage.png';
+import IPhoneImage from '@/public/iphoneimage.png';
 
 export default function HeroSection() {
   return (
