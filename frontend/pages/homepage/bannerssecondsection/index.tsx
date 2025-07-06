7302be4863b646427b1aaeb6335b38fc0ff36b52
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-07 00:23:15 +0400

    BannersSecondSection section added pages/homepage folder

---
+++ frontend/pages/homepage/bannerssecondsection/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import BannerSecondImage from '@/public/bannersecond.png';
+
+export default function BannersSecondSection() {
+  return (
+    <section className='w-full mt-20'>
+      <div className='relative'>
+        <picture className='w-full relative h-64'>
+          <Image src={BannerSecondImage} width={6000} height={448} quality={100} alt='Banner 2 Image'/>
+        </picture>
+        <article className='absolute top-2/5 bottom-0 translate-x-0 left-0 right-0 text-center'>
+          <h3 className='text-white text-[4.5rem] font-thin leading-[4.5rem]'>Big Summer <span className='font-medium'>Sale</span></h3>
+          <p className='text-sonic-silver text-[1rem] leading-8 pb-10'>Commodo fames vitae vitae leo mauris in. Eu consequat.</p>
+          <Button variant={"outline"} size={"lg"}>Shop Now</Button>
+        </article>
+      </div>
+    </section>
+  )
+}
