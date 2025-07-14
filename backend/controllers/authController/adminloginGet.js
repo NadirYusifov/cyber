186505c3734e4798ee95710c46ae07b5a386f56c
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-15 01:38:17 +0400

    adminloginGet change

--- backend/controllers/authController/adminloginGet.js
+++ backend/controllers/authController/adminloginGet.js
@@ -1,38 +1,45 @@
-import bcyrpt from "bcrypt";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { cyberauthModels } from "../../models/cyberModels.js";
 
 export const adminloginGet = async (req, res) => {
   dotenv.config();
+  const { username, password } = req.query;
 
-  const { username, password } = req.body;
   try {
-    const isUsername = await cyberauthModels.findOne({ username: "admin" });
+    const isUsername = await cyberauthModels.findOne({ username });
 
-    if (isUsername) {
-      bcyrpt.compare(password, isUsername.password, (error, result) => {
-        if (result) {
-          const token = jwt.sign({ username: isUsername.username }, process.env.JWT_SECURITY_KEY, { expiresIn: "1h" })
-          res.status(200).json({
-            status: 200,
-            message: "Login successfully",
-            success: true,
-            token: token
-          })
-        } else {
-          res.status(401).json({
-            status: 401,
-            message: "Unauth",
-            success: false
-          });
-        };
+    if (!isUsername) {
+      res.status(404).json({
+        status: 404,
+        message: "User not found",
+        success: false
       });
-    };
+      return;
+    }
+
+    const passwordcompare = bcrypt.compare(password, isUsername.password);
+    if (!passwordcompare) {
+      res.status(401).json({
+        status: 401,
+        message: "Password incorrect",
+        success: false
+      });
+      return;
+    }
+
+    const token = jwt.sign({ username: isUsername.username }, process.env.JWT_SECURITY_KEY, { expiresIn: "1h" })
+
+    res.status(200).json({
+      status: 200,
+      message: "Login successfully",
+      success: true,
+      token: token
+    });
   } catch (error) {
     res.status(500).json({
       status: 500,
-      message: "error",
       error: error.message,
     });
   };
