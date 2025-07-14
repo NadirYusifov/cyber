00bcece3b46b03cc878942c42927dfd4940618b5
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-15 01:40:49 +0400

    adminlogin changed

--- frontend/app/(admin)/admin/[adminlogin]/page.tsx
+++ frontend/app/(admin)/admin/[adminlogin]/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
+import { useState } from "react";
+// import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { CiLock, CiUnlock } from "react-icons/ci";
-import { useState } from "react";
 
 export default function AdminLogin() {
-  const [password, setPassword] = useState(false)
+  // const router = useRouter()
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showpassword, setShowPassword] = useState(false);
 
   return (
     <section className="h-screen flex justify-center items-center">
@@ -15,11 +20,11 @@ export default function AdminLogin() {
           <article className="pb-10 text-center">
             <h3 className="text-[2.188rem] font-medium">Admin Login</h3>
           </article>
-          <form onSubmit={(e) => e.preventDefault()} className="flex flex-col justify-center space-y-4">
-            <Input className="py-5" type="text" placeholder="Username" />
+          <form className="flex flex-col justify-center space-y-4">
+            <Input value={username} name="username" onChange={(e) => setUsername(e.target.value)} className="py-5" type="text" placeholder="Username" required />
             <div className="relative">
-              <Input className="relative py-5" type={!password ? "password" : "text"} placeholder="Password" />
-              <button type="button" onClick={() => setPassword(!password)} className="text-[1.25rem] absolute top-0 right-5 translate-y-1/2 cursor-pointer">{!password ? <CiLock /> : <CiUnlock />}</button>
+              <Input className="relative py-5" name="password" value={password} onChange={(e) => setPassword(e.target.value)} type={!showpassword ? "password" : "text"} placeholder="Password" required />
+              <button type="button" onClick={() => setShowPassword(!showpassword)} className="text-[1.25rem] absolute top-0 right-5 translate-y-1/2 cursor-pointer">{!showpassword ? <CiLock /> : <CiUnlock />}</button>
             </div>
             <Button className="text-[1.125rem] py-5">Login</Button>
           </form>
