cffc6de4e8880ae10b32c65d03049fe1be6bd281
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-27 23:31:51 +0400

    sidebar inside function changed

--- frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
+++ frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
@@ -1,18 +1,32 @@
-import { CirclePlus, House, Settings } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+"use client"
 
-export default function Sidebar({children}: any) {
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import cyberlogo from "@/public/cyberlogo.png";
+import { CirclePlus, House, Settings } from 'lucide-react';
+
+export default function Sidebar() {
+  const [menu, setMenu] = useState(false)
+
   return (
-    <aside className="bg-dark-charcoal text-white max-w-xs h-screen">
-      <div className="container mx-auto px-4">
-        <article className="text-[2.5rem]">
-          <h3>Admin Panel</h3>
-        </article>
+    <aside className="bg-dark-charcoal text-white w-xs size-full h-screen">
+      <div className="container mx-auto px-4 mt-10">
+        <picture className="flex items-center justify-center">
+          <Image src={cyberlogo} width={100} height={100} quality={100} alt="cyber logo" />
+        </picture>
         <article className="text-[1.3rem] flex flex-col space-y-3 pt-10">
-          <Link className="flex items-center" href={"/admin/home"}><House />Home</Link>
-          <Link className="flex items-center" href={"/admin/home/addproduct"}><CirclePlus />Add Product</Link>
-          <Link className="flex items-center" href={"/admin/home/settings"}><Settings />Settings</Link>
+          <Link className="flex items-center" href={"/admin/home"}>
+            <span className="pr-2"><House /></span>
+            Home
+          </Link>
+          <Link className="flex items-center" href={"/admin/home/addproduct"}>
+            <span className="pr-2"><CirclePlus /></span>
+            Add Product</Link>
+          <Link className="flex items-center" href={"/admin/home/settings"}>
+            <span className="pr-2"><Settings /></span>
+            Settings
+          </Link>
         </article>
       </div>
     </aside>
