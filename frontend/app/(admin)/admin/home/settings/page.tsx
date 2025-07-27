561cc183b649e19085694a7256d51b4281ed008b
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-27 23:30:12 +0400

    settings added

---
+++ frontend/app/(admin)/admin/home/settings/page.tsx
@@ -0,0 +1,29 @@
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function SettingsPage() {
+  return (
+    <section className="w-full mt-5">
+      <div className="container mx-auto px-4">
+        <article className="text-[3rem] font-medium mb-10">
+          <h3>Settings</h3>
+        </article>
+        <article className="flex flex-col flex-wrap space-y-2">
+          <Link className="text-[1.3rem] flex items-center justify-between w-full hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/profile"}>
+            Profile
+            <span><ChevronRight /></span>
+          </Link>
+          <Link className="text-[1.3rem] flex items-center justify-between w-full hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/profileedit"}>
+            Profile Edit
+            <span><ChevronRight /></span>
+          </Link>
+          <Link className="text-[1.3rem] flex items-center justify-between w-full hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/theme"}>
+            Theme
+            <span><ChevronRight /></span>
+          </Link>
+        </article>
+      </div>
+    </section>
+  )
+}
