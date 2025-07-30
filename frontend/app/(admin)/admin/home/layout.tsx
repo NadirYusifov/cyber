4dbbc140869bb42b70aa1230b9db1ced6731b8b8
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-31 00:38:06 +0400

    font, useState, useEffect, ThemeProviders added

--- frontend/app/(admin)/admin/home/layout.tsx
+++ frontend/app/(admin)/admin/home/layout.tsx
@@ -1,6 +1,16 @@
-import { ThemeProvider } from "next-themes";
+"use client"
+
+import { useEffect, useState } from "react";
 import "../../../globals.css";
 import Sidebar from "./sidebar/sidebar";
+import ThemeProviders from "@/components/providers/themeprovider";
+import { Inter } from "next/font/google";
+
+const interSans = Inter({
+  weight: "variable",
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,12 +18,24 @@ export default function RootLayout({
   children: React.ReactNode,
 }>) {
 
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true);
+  })
+
   return (
-    <main className="admin-layout flex w-full">
-      <Sidebar />
-      <ThemeProvider enableSystem={false} attribute={"class"} defaultTheme="system">
-        {children}
-      </ThemeProvider>
-    </main>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={`${interSans.className} antialiased`}>
+        {isClient &&
+          <main className="admin-layout-main flex">
+            <Sidebar />
+            <ThemeProviders enableSystem attribute="class" defaultTheme="system">
+              {children}
+            </ThemeProviders>
+          </main>
+        }
+      </body>
+    </html>
   );
 }
