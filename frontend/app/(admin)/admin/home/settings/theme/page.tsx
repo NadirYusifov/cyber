e7176b9cd02664af2396d8b1727a19bdafa43925
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-07-31 00:40:42 +0400

    import order corrected: moved lucide-react imports below useTheme

--- frontend/app/(admin)/admin/home/settings/theme/page.tsx
+++ frontend/app/(admin)/admin/home/settings/theme/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { Monitor, Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
+import { Monitor, Moon, Sun } from 'lucide-react';
 
 export default function Theme() {
     const { resolvedTheme, theme, setTheme } = useTheme();
