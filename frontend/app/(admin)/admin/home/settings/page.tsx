6d9814d3827292b82f2e64f2df80048babee9cf6
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-05 23:17:01 +0400

    settings page next-intl added

--- frontend/app/(admin)/admin/home/settings/page.tsx
+++ frontend/app/(admin)/admin/home/settings/page.tsx
@@ -1,25 +1,27 @@
-import { ChevronRight, Pencil, SunMoon } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import { ChevronRight } from 'lucide-react';
+import { getTranslations } from 'next-intl/server';
 
-export default function SettingsPage() {
+export default async function SettingsPage() {
+const t = await getTranslations("Settings")
+
   return (
     <section className="w-full mt-5">
       <div className="container mx-auto px-4">
         <article className="text-[3rem] font-medium mb-10">
-          <h3>Settings</h3>
+          <h3>{t("settings")}</h3>
         </article>
         <article className="flex flex-col flex-wrap space-y-2">
           <Link className="text-[1.3rem] flex items-center justify-between w-full active:text-white active:bg-dark-charcoal hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/profile"}>
-            Profile
+            {t("profile")}
             <span><ChevronRight /></span>
           </Link>
           <Link className="text-[1.3rem] flex items-center justify-between w-full active:text-white active:bg-dark-charcoal hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/profileedit"}>
-            Profile Edit
+            {t("profileedit")}
             <span><ChevronRight /></span>
           </Link>
-          <Link className="text-[1.3rem] flex items-center justify-between w-full active:text-white focus:bg-dark-charcoal hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/theme"}>
-            Theme
+          <Link className="text-[1.3rem] flex items-center justify-between w-full active:text-white focus:bg-dark-charcoal hover:bg-dark-charcoal hover:text-white px-2 py-1.5 rounded-md transition-all" href={"/admin/home/settings/prefences"}>
+            {t("prefences")}
             <span><ChevronRight /></span>
           </Link>
         </article>
