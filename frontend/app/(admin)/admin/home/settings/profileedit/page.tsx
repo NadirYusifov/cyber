ebbae2e034f7945230f3ac18e8a5a423b6a75f31
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-07 01:20:05 +0400

    profileedit ui ready but css not ready and next-intl added

--- frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
+++ frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react';
+import { useTranslations } from 'next-intl';
+import { Button } from '@/components/ui/button';
+import { AvatarImage } from '@radix-ui/react-avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Lock, Unlock } from 'lucide-react';
 
 export default function ProfileEdit() {
+  const t = useTranslations("ProfileEdit");
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>ProfileEdit</div>
+    <section className="">
+      <div className="container mx-auto px-4">
+        <article>{t("profileedit")}</article>
+        <Avatar>
+          <AvatarImage />
+          <AvatarFallback>a</AvatarFallback>
+        </Avatar>
+          <form onChange={(e) => e.preventDefault()} className="relative">
+            <label htmlFor="Name">{t("name")}:</label>
+            <input type="text" />
+            <div className="relative">
+              <label htmlFor="Password">{t("password")}:</label>
+              <input className="relative outline-1" type={showPassword ? "text" : "password"} />
+              <button onClick={() => setShowPassword(!showPassword)} className="absolute top-0.5 right-2" type="button">{showPassword ? <Unlock size={18}/> : <Lock size={18}/>}</button>
+            </div>
+            <div className="flex items-center gap-x-3">
+            <Button variant={"destructive"}>{t("cancel")}</Button>
+            <Button>{t("save")}</Button>
+            </div>
+          </form>
+      </div>
+    </section>
   )
 }
