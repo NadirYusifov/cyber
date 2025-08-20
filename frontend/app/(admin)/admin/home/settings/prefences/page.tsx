102915f7a59ec277f262a74fef71d88727ac5201
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-21 00:44:31 +0400

    ThemeSwitch and LanguageSwitch to seperate components

--- frontend/app/(admin)/admin/home/settings/prefences/page.tsx
+++ frontend/app/(admin)/admin/home/settings/prefences/page.tsx
@@ -1,63 +1,19 @@
-"use client"
-
-import { useTheme } from 'next-themes';
-import setCookie from '@/servers/cookie';
-import { useEffect, useState } from 'react';
-import { useLocale, useTranslations } from 'next-intl';
-import { Monitor } from 'lucide-react';
-// import { Monitor } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+import LanguageSwitch from './@languageswitch/page';
+import ThemeSwitch from './@themeswitch/page';
 
 export default function Theme() {
-    const { theme, setTheme } = useTheme();
-    const [mounted, setMounted] = useState(false);
     const t = useTranslations("Prefences");
-    const local = useLocale();
-
-    const handleselectLang = (e: any) => {
-        e.preventDefault();
-        setCookie("LANGUAGE", e.target.value);
-    }
-
-    useEffect(() => {
-        setMounted(true)
-    }, []);
-
-    if (!mounted) {
-        return (
-            <div>
-                <p>Loading...</p>
-            </div>
-        )
-    };
 
     return (
-        <section className="themes-section w-full mt-5">
+        <main className="themes-section w-full mt-5">
             <div className="container mx-auto px-4">
-                <article className="themes-section-header">
-                    <h3 className="text-[3rem] font-medium mb-10">{t("prefences")}</h3>
+                <article className="themes-section-header mb-10">
+                    <h3 className="text-[3rem] font-medium">{t("prefences")}</h3>
                 </article>
-                <div className="theme-select w-full flex items-center justify-between">
-                    <article className="themes-select-content">
-                        <p className="text-[1.275rem]">{t("themesselect")}</p>
-                    </article>
-                    <div className="themes-change">
-                        <select className="themes-select w-36 outline-none border border-zinc-500 rounded-sm px-2 py-1" value={theme} onChange={e => setTheme(e.target.value)}>
-                            <option value="system">{t("system")}</option>
-                            <option value="light">{t("light")}</option>
-                            <option value="dark">{t("dark")}</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="flex items-center justify-between mt-3">
-                    <article>
-                        <p className="text-[1.275rem]">{t("langselect")}</p>
-                    </article>
-                    <select className="w-36 outline-none border border-zinc-500 rounded-sm px-2 py-1" defaultValue={local} onChange={handleselectLang}>
-                        <option value="az">Azərbaycanca</option>
-                        <option value="en">Engilsh</option>
-                    </select>
-                </div>
+                <ThemeSwitch />
+                <LanguageSwitch />
             </div>
-        </section>
+        </main>
     )
 }
