1c325f30b12d416d134ab3f8c423c31b82fed4d2
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-21 00:45:21 +0400

    sidebar littlesidebar for the some functions added

--- frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
+++ frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
@@ -1,52 +1,63 @@
-// "use client"
+"use client"
 
 import Link from 'next/link';
-// import Image from 'next/image';
-// import { useState } from 'react';
-// import cyberlogo from "@/public/cyberlogo.png";
+import { useState } from 'react';
 import { useTranslations } from 'next-intl';
-import { CirclePlus, House, LogOut, Settings } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { CirclePlus, House, LogOut, Settings, SquareChevronLeft, SquareChevronRight } from 'lucide-react';
+import SidebarDropMenu from '@/components/common/sidebardropmenu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
+
+
 export default function Sidebar() {
-  // const [menu, setMenu] = useState(false);
-  const t = useTranslations("Sidebar")
+  const pathname = usePathname();
+  const t = useTranslations("Sidebar");
+  const [largesidebarmenu, setLargeSidebarMenu] = useState(true);
 
   return (
-    <aside className="w-sm size-full h-screen border-r-2 border-zinc-700">
-      <div className="container mx-auto px-4 mt-10">
-        {/* <picture className="flex items-center justify-center">
-          <Image src={cyberlogo} width={100} height={100} quality={100} alt="cyber logo" />
-        </picture> */}
-        <article>
-          <h3 className="text-[2rem] font-bold">Admin Panel</h3>
-        </article>
-        <article className="text-[1.3rem] flex flex-col space-y-3 pt-10">
-          <Link className="flex items-center" href={"/admin/home"}>
-            <span className="pr-2"><House /></span>
-            {t("home")}
-          </Link>
-          <Link className="flex items-center" href={"/admin/home/addproduct"}>
-            <span className="pr-2"><CirclePlus /></span>
-            {t("addproducts")}
-          </Link>
-          <Link className="flex items-center" href={"/admin/home/settings"}>
-            <span className="pr-2"><Settings /></span>
-            {t("settings")}
-          </Link>
-        </article>
-
-        <div className="mt-[42rem] flex items-center gap-x-3">
-          <Avatar className="bg-dark-charcoal border border-zinc-500 w-11 h-11">
-            <AvatarImage />
-            <AvatarFallback>a</AvatarFallback>
-          </Avatar>
-          <article>
-            <Link href={"/admin/home/settings/profile"}>admin</Link>
-            <Link className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-500/50 hover:underline transition-all" href={""}>{t(("logout"))}<LogOut size={16} /></Link>
+    <>
+      {/* {largesidebarmenu } */}
+      <aside className={`flex border-r-2 border-zinc-700 h-screen transition-all duration-400 ease-in-out ${!largesidebarmenu ? "w-[5rem]" : "w-sm"}`}>
+        <div className="container mx-auto px-4 mt-10">
+          <div className={`pb-6 flex ${!largesidebarmenu ? "justify-center items-center" : "justify-end"}`}>
+            <button className="" onClick={() => setLargeSidebarMenu(!largesidebarmenu)}>
+              <span>{!largesidebarmenu ? <SquareChevronLeft /> : <SquareChevronRight />}</span>
+            </button>
+          </div>
+          <article className="text-center">
+            <h3 className="text-[2rem] font-bold">{!largesidebarmenu ? "A" : "Admin Panel"}</h3>
           </article>
+          <article className="text-[1.3rem] flex flex-col space-y-3 pt-10">
+            <Link className={`flex items-center transition-all ease-in hover:bg-dark-charcoal hover:text-white text px-2 py-1 rounded-sm ${!largesidebarmenu && "justify-center"} ${pathname == "/admin/home" ? "bg-dark-charcoal text-white px-2 py-1 rounded-sm" : ""}`} href={"/admin/home"}>
+              <span title="Dashboard" className={`${largesidebarmenu && "pr-2"}`}><House /></span>
+              <p className={`${!largesidebarmenu && "sr-only"}`}>{t("home")}</p>
+            </Link>
+            <Link className={`flex items-center transition-all ease-in hover:bg-dark-charcoal hover:text-white text px-2 py-1 rounded-sm ${!largesidebarmenu && "justify-center "} ${!largesidebarmenu && pathname == "/admin/home/addproduct" ? "bg-dark-charcoal text-white px-2 py-1 rounded-sm" : ""}`} href={"/admin/home/addproduct"}>
+              <span title="Add Products" className={`${largesidebarmenu && "pr-2"}`}><CirclePlus /></span>
+              <p className={`${!largesidebarmenu && "sr-only"}`}>{t("addproducts")}</p>
+            </Link>
+            <Link className={`flex items-center transition-all ease-in hover:bg-dark-charcoal hover:text-white text px-2 py-1 rounded-sm ${!largesidebarmenu && "justify-center"} ${pathname == "/admin/home/settings" ? "bg-dark-charcoal text-white px-2 py-1 rounded-sm" : ""}`} href={"/admin/home/settings"}>
+              <span title="Settings" className={`${largesidebarmenu && "pr-2"}`}><Settings /></span>
+              <p className={`${!largesidebarmenu && "sr-only"}`}>{t("settings")}</p>
+            </Link>
+          </article>
+          {
+            !largesidebarmenu ? <SidebarDropMenu t={t} /> :
+              <div className="mt-[39rem] flex items-center gap-x-3">
+
+                <Avatar className="bg-dark-charcoal border border-zinc-500 w-11 h-11">
+                  <AvatarImage />
+                  <AvatarFallback>a</AvatarFallback>
+                </Avatar>
+                <article className="flex flex-col">
+                  <Link href={"/admin/home/settings/profile"}>admin</Link>
+                  <Link className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={""}>{t(("logout"))}<LogOut size={16} /></Link>
+                </article>
+              </div>
+          }
         </div>
-      </div>
-    </aside>
+      </aside>
+    </>
   )
 }
