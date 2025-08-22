d4a3b53ff0a3d0ed389d30059e1d89b9b2a9c0a4
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-22 23:45:54 +0400

    adminauth rename auth

--- backend/models/cyberModels.js
+++ backend/models/cyberModels.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 import { cyberauthSchema } from "../schemas/cyberSchema.js";
 import { cyberproductsSchema } from "../schemas/cyberSchema.js";
 
-export const cyberauthModels = mongoose.model("adminauth", cyberauthSchema);
+export const cyberauthModels = mongoose.model("auth", cyberauthSchema);
 export const cyberproductsModels = mongoose.model("productslist", cyberproductsSchema);
