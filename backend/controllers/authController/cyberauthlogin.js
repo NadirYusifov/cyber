f84cf6bb40927490ec35807914f5dba1628c73ae
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-22 23:44:35 +0400

    cyberauthlogin added

---
+++ backend/controllers/authController/cyberauthlogin.js
@@ -0,0 +1,46 @@
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { cyberauthModels } from "../../models/cyberModels.js";
+
+export const cyberauthlogin = async (req, res) => {
+  dotenv.config();
+
+  const { email, password } = req.body;
+
+  const user = await cyberauthModels.findOne({ email });
+  if (!user) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  const matchpassword = await bcrypt.compare(password, user.password);
+  if (!matchpassword) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      message: "Password is incorrect",
+    });
+  }
+
+  const token = await jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECURITY_KEY,
+    { expiresIn: "30d" },
+  );
+
+  res.status(200).json({
+    token,
+    status: 200,
+    success: true,
+    message: "Authentication",
+    user: {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
+};
