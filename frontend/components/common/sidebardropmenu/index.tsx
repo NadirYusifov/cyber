fe6a6b23bf967b7224773b18406036134b0d16b6
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-22 14:51:02 +0400

    dropdownmenu line clean and tidied up

--- frontend/components/common/sidebardropmenu/index.tsx
+++ frontend/components/common/sidebardropmenu/index.tsx
@@ -1,29 +1,27 @@
+import Link from 'next/link';
+import { LogOut } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { LogOut } from 'lucide-react';
-import Link from 'next/link';
 
 export default function SidebarDropMenu({ t }: any) {
     return (
         <DropdownMenu>
-            <div className="mt-[39rem] flex">
-                <DropdownMenuTrigger>
-                    <Avatar className="bg-dark-charcoal border border-zinc-500 w-11 h-11">
-                        <AvatarImage />
-                        <AvatarFallback>a</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>{t("profile")}</DropdownMenuLabel>
-                    <DropdownMenuItem>
-                        <Link href={"/admin/home/settings/profile"}>admin</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                        <Link className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={""}>{t(("logout"))}<LogOut size={16} /></Link>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </div>
+            <DropdownMenuTrigger className="mt-[38rem]">
+                <Avatar className="bg-dark-charcoal border border-zinc-500 w-11 h-11">
+                    <AvatarImage />
+                    <AvatarFallback>a</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>{t("profile")}</DropdownMenuLabel>
+                <DropdownMenuItem>
+                    <Link href={"/admin/home/settings/profile"}>admin</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={""}>{t(("logout"))}<LogOut size={16} /></Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     )
 }
