6b3b99689a341b48e4b11ef687fcd3241f3ab452
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-23 22:23:25 +0400

    auth.js status 401 added

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,29 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import { cyberauthModels } from "../models/cyberModels.js";
 
 export const authmiddleware = async (req, res, next) => {
   dotenv.config();
 
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) {
-    res.status(401).json({
-      status: 401,
-      message: "No token, access is prohibited",
-    });
-    return;
-  }
-
   try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      res.status(401).json({
+        status: 401,
+        message: "No token, access is prohibited",
+      });
+      return;
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECURITY_KEY);
-    req.user = decoded;
+    req.user = await cyberauthModels.findById(decoded.id).select("-password");
+    if (!req.user) {
+      res.status(401).json({
+        status: 401,
+        message: "User not found",
+      });
+    }
+
     next();
   } catch (error) {
     res.status(403).json({
@@ -24,15 +32,4 @@ export const authmiddleware = async (req, res, next) => {
     });
     return;
   }
-};
-
-export const requireadmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    res.status(403).json({
-      status: 403,
-      message: "Admin access only",
-    });
-    return;
-  }
-  next();
 };
