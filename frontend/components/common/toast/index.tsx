bfeeef059414e323640625c2dcf4285f3691e2ad
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-08-31 01:41:58 +0400

    Toaster added closeButton

--- frontend/components/common/toast/index.tsx
+++ frontend/components/common/toast/index.tsx
@@ -3,7 +3,7 @@ import { Toaster } from 'sonner'
 export default function Toast() {
     return (
         <div>
-            <Toaster richColors expand={false} position='top-center'/>
+            <Toaster richColors closeButton expand={false} position='top-center'/>
         </div>
     )
 }
