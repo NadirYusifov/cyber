73fe7eb35e246f6465a4fc7bb2f8cae94dbcaa92
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-02 02:21:38 +0400

    commit and cookieParser added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import { cyberauthRoutes } from "./routes/cyberauthRoutes.js";
 import { cyberproductsRoutes } from "./routes/cyberproductsRoutes.js";
 
@@ -11,6 +12,7 @@ dotenv.config();
 const PORT = 7575;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -18,9 +20,11 @@ app.use(
   }),
 );
 
+// Routes
 app.use("/auth", cyberauthRoutes);
 app.use("/products", cyberproductsRoutes);
 
+// Mongoose connect
 const mongoconnect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI).then(() => {
@@ -32,6 +36,7 @@ const mongoconnect = async () => {
 };
 console.log(mongoconnect());
 
+// Server running
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
