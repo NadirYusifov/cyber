31c9cea577b421248eb92acbcbffa14b15f78df9
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-02 02:23:55 +0400

    RootLayout component refactored to remove unused internationalization logic and simplify structure

--- frontend/app/(admin)/admin/home/layout.tsx
+++ frontend/app/(admin)/admin/home/layout.tsx
@@ -1,17 +1,8 @@
 import "@/app/globals.css";
 import Sidebar from "./sidebar/sidebar";
-import { Inter } from "next/font/google";
-import { NextIntlClientProvider } from "next-intl";
-import { getLocale, getMessages } from "next-intl/server";
 import ThemeProviders from "@/components/providers/themeprovider";
 
 // import {onCLS} from "web-vitals"
-
-const interSans = Inter({
-  weight: "variable",
-  variable: "--font-inter-sans",
-  subsets: ["latin"],
-});
 
 export default async function RootLayout({
   children,
@@ -19,22 +10,12 @@ export default async function RootLayout({
   children: React.ReactNode,
 }>) {
 
-  // onCLS(console.log)
-  const locale = await getLocale();
-  const messages = await getMessages();
-
   return (
-    <html lang={locale} suppressHydrationWarning={true}>
-      <body className={`${interSans.className} antialiased`} suppressHydrationWarning={true}>
-        <main className="admin-layout-main flex">
-          <ThemeProviders enableSystem={true} attribute="class" defaultTheme="system">
-            <NextIntlClientProvider messages={messages}>
-              <Sidebar />
-              {children}
-            </NextIntlClientProvider>
-          </ThemeProviders>
-        </main>
-      </body>
-    </html>
+    <main className="admin-layout-main flex">
+      <ThemeProviders enableSystem={true} attribute="class" defaultTheme="system">
+        <Sidebar />
+        {children}
+      </ThemeProviders>
+    </main>
   );
 }
