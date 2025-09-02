74a7618bee024c61c1ad4cc2b9b409ac38e67f5a
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-03 02:30:15 +0400

    sidebar include added toast and handlelogout

--- frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
+++ frontend/app/(admin)/admin/home/sidebar/sidebar.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Link from 'next/link';
+import { toast } from 'sonner';
+import Toast from '@/components/common/toast';
 import { useState } from 'react';
 import { useTranslations } from 'next-intl';
 import { usePathname, useRouter } from 'next/navigation';
@@ -13,6 +15,26 @@ export default function Sidebar() {
   const pathname = usePathname();
   const t = useTranslations("Sidebar");
   const [largesidebarmenu, setLargeSidebarMenu] = useState(true);
+
+  const handlelogout = async () => {
+    try {
+      const res = await fetch("http://localhost:7575/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+      if (res.ok) {
+        router.push("/admin/adminlogin");
+        toast.success("Logout succesfull")
+      }
+    } catch (err) {
+      if (err) {
+        toast.error("Have a problem with the server")
+      }
+    }
+  }
 
   return (
     <aside className={`flex border-r-2 border-zinc-700 size-full h-screen transition-all duration-400 ease-in-out ${!largesidebarmenu ? "w-[5rem]" : "w-sm"}`}>
@@ -48,11 +70,12 @@ export default function Sidebar() {
               </Avatar>
               <article className="flex flex-col">
                 <Link href={"/admin/home/settings/profile"}>admin</Link>
-                {router && <Link className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={"/admin/adminlogin"}>{t(("logout"))}<LogOut size={16} /></Link>}
+                <Link onClick={handlelogout} className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={""}>{t(("logout"))}<LogOut size={16} /></Link>
               </article>
             </div>
         }
       </div>
+      <Toast />
     </aside>
   )
 }
