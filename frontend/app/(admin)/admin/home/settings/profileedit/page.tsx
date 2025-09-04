cf8a52fc46b64b4c711b98dde16ba313d35103f3
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-05 01:54:41 +0400

    preofileedit page onSubmit removed

--- frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
+++ frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { AvatarImage } from '@radix-ui/react-avatar';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Lock, Unlock } from 'lucide-react';
+import Toast from '@/components/common/toast';
 
 export default function ProfileEdit() {
   const t = useTranslations("ProfileEdit");
@@ -24,18 +25,19 @@ export default function ProfileEdit() {
             <AvatarImage />
             <AvatarFallback className="bg-dark-charcoal text-white text-[1.6rem]">a</AvatarFallback>
           </Avatar>
-          <form onChange={(e) => e.preventDefault()} className="relative w-full flex flex-col lg:gap-x-3 pt-7">
-            <label htmlFor="Name">{t("name")}:</label>
-            <input className="outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type="text" />
+          <form className="relative w-full flex flex-col lg:gap-x-3 pt-7">
+            <label htmlFor="Email">{t("name")}:</label>
+            <input onChange={(e) => e.target.value} className="outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type="email" />
             <div className="relative mt-5">
               <label htmlFor="Password">{t("password")}:</label>
-              <input className="relative w-full outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type={showPassword ? "text" : "password"} />
+              <input onChange={(e) => e.target.value} className="relative w-full outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type={showPassword ? "text" : "password"} />
               <button onClick={() => setShowPassword(!showPassword)} className="absolute right-3 translate-y-1/2" type="button">{showPassword ? <Unlock size={18} /> : <Lock size={18} />}</button>
             </div>
             <div className="flex items-center gap-x-3 mt-5">
               <Button className="text-black dark:text-white" variant={"outline"}>{t("cancel")}</Button>
               <Button>{t("save")}</Button>
             </div>
+            <Toast />
           </form>
         </div>
       </div>
