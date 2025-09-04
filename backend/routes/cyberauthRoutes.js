17959fe958c1621959c965e24b7a1fae0e16886d
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-05 01:51:09 +0400

    patch added

--- backend/routes/cyberauthRoutes.js
+++ backend/routes/cyberauthRoutes.js
@@ -4,6 +4,7 @@ import { authmiddleware } from "../middleware/auth.js";
 import { cyberauthlogin } from "../controllers/authController/cyberauthlogin.js";
 import { cyberauthsignup } from "../controllers/authController/cyberauthsingup.js";
 import { cyberauthlogout } from "../controllers/authController/cyberauthlogout.js";
+import { cyberauthprofileedit } from "../controllers/authController/cyberauthprofileedit.js";
 
 export const cyberauthRoutes = Router();
 
@@ -13,7 +14,11 @@ cyberauthRoutes.post("/login", cyberauthlogin);
 // POST method for Signup
 cyberauthRoutes.post("/signup", cyberauthsignup);
 
+// POST nethod for Logout
 cyberauthRoutes.post("/logout", cyberauthlogout);
 
+cyberauthRoutes.patch("/profileedit/:id", cyberauthprofileedit);
+
+// GET methof for private route
 cyberauthRoutes.get("/private", authmiddleware, checkrole(["admin"]), (req, res) => { res.json("Welcome admin") });
 
