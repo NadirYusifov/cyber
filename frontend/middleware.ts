3cae82b01a4f71a29380f45f3d46c9a294234dd3
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-05 01:52:01 +0400

    middelware pathname admin write

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     if (req.nextUrl.pathname.startsWith("/admin/home")) {
         const token = req.cookies.get("cybercookie")?.value
 
         if (!token) {
-            return NextResponse.redirect(new URL("adminlogin", req.url))
+            if (req.nextUrl.pathname === "adminlogin") {
+                return NextResponse.next();
+            }
+            return NextResponse.redirect(new URL("adminlogin", req.url));
         }
     }
 
@@ -13,5 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: "/admin/home/:path*"
+    matcher: ["/admin/home/:path*"]
 }
