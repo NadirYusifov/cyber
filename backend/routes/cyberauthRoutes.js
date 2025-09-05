1a058f360bcbecff856f3c38daa8c0d25cec7225
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-06 01:32:11 +0400

    GET method with profile added and commit added with PATCH method

--- backend/routes/cyberauthRoutes.js
+++ backend/routes/cyberauthRoutes.js
@@ -4,6 +4,7 @@ import { authmiddleware } from "../middleware/auth.js";
 import { cyberauthlogin } from "../controllers/authController/cyberauthlogin.js";
 import { cyberauthsignup } from "../controllers/authController/cyberauthsingup.js";
 import { cyberauthlogout } from "../controllers/authController/cyberauthlogout.js";
+import { cyberauthprofile } from "../controllers/authController/cyberauthprofile.js";
 import { cyberauthprofileedit } from "../controllers/authController/cyberauthprofileedit.js";
 
 export const cyberauthRoutes = Router();
@@ -17,7 +18,11 @@ cyberauthRoutes.post("/signup", cyberauthsignup);
 // POST nethod for Logout
 cyberauthRoutes.post("/logout", cyberauthlogout);
 
+// PATCH method for profile edit
 cyberauthRoutes.patch("/profileedit/:id", cyberauthprofileedit);
+
+// GET method for profile get
+cyberauthRoutes.get("/profile", cyberauthprofile);
 
 // GET methof for private route
 cyberauthRoutes.get("/private", authmiddleware, checkrole(["admin"]), (req, res) => { res.json("Welcome admin") });
