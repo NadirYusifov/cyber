3623da1d28cd926f8c0f951cb75cbfcec0b7aa10
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-10 23:44:46 +0400

    sidebar dropdownmenu interface and map used

--- frontend/components/common/sidebardropmenu/index.tsx
+++ frontend/components/common/sidebardropmenu/index.tsx
@@ -3,10 +3,16 @@ import { LogOut } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
-export default function SidebarDropMenu({ t, handlelogout }: any) {
+interface DropdownMenuUserList {
+    t:(key: string) => string;
+    handlelogout: () => void;
+    user: { _id: string, email: string }[],
+}
+
+export default function SidebarDropMenu({ t, handlelogout, user }: DropdownMenuUserList) {
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger className="mt-[38rem]">
+            <DropdownMenuTrigger className="mt-[30rem]">
                 <Avatar className="bg-dark-charcoal border border-zinc-500 w-11 h-11">
                     <AvatarImage />
                     <AvatarFallback>a</AvatarFallback>
@@ -14,12 +20,14 @@ export default function SidebarDropMenu({ t, handlelogout }: any) {
             </DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuLabel>{t("profile")}</DropdownMenuLabel>
-                <DropdownMenuItem>
-                    <Link href={"/admin/home/settings/profile"}>admin</Link>
-                </DropdownMenuItem>
+                {user.map((u, _id) => (
+                    <DropdownMenuItem key={_id}>
+                        <Link href={"/admin/home/settings/profile"}>{u.email}</Link>
+                    </DropdownMenuItem>
+                ))}
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                    <Link onClick={handlelogout} className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={{pathname: "/admin/adminlogin"}}>{t(("logout"))}<LogOut size={16} /></Link>
+                    <Link onClick={handlelogout} className="flex items-center gap-x-2 text-zinc-500 hover:text-zinc-700 hover:underline transition-all" href={{ pathname: "/admin/adminlogin" }}>{t(("logout"))}<LogOut size={16} /></Link>
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
