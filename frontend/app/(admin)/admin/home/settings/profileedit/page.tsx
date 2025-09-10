eb10eafdfa7b970b49d834bfb22732cf1dfaa88b
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-11 01:58:28 +0400

    profileedit updated but update just not working

--- frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
+++ frontend/app/(admin)/admin/home/settings/profileedit/page.tsx
@@ -1,16 +1,62 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { ReactEventHandler, useEffect, useState } from 'react';
+import { jwtDecode } from "jwt-decode";
 import { useTranslations } from 'next-intl';
+import { Lock, Unlock } from 'lucide-react';
+import Toast from '@/components/common/toast';
 import { Button } from '@/components/ui/button';
 import { AvatarImage } from '@radix-ui/react-avatar';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { Lock, Unlock } from 'lucide-react';
-import Toast from '@/components/common/toast';
+import { toast } from 'sonner';
+
+interface tokenpayload {
+  userId: string
+}
 
 export default function ProfileEdit() {
+
+
   const t = useTranslations("ProfileEdit");
+  const [editprofile, setEditProfile] = useState({ email: "", password: "" });
+  const [userId, setUserId] = useState<string | null>(null)
   const [showPassword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("cybercookie");
+    if (token) {
+      const decoded: tokenpayload = jwtDecode(token);
+      setUserId(decoded.userId)
+    }
+  }, [])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditProfile({ ...editprofile, [e.target.name]: e.target.value })
+  };
+
+  const handleSumbit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (userId) return;
+
+    const token = localStorage.getItem("cybercookie")
+
+    const res = await fetch(`http://localhost:7575/auth/profileedit/${userId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer: ${token}`
+      },
+      body: JSON.stringify(editprofile)
+    })
+    if (res.ok) {
+      const data = res.json()
+      toast.success("Update successfully.")
+      console.log(data);
+    } else {
+      toast.error("Error updating profile")
+    }
+  };
+    
 
   return (
     <section className="w-full mt-5">
@@ -25,12 +71,12 @@ export default function ProfileEdit() {
             <AvatarImage />
             <AvatarFallback className="bg-dark-charcoal text-white text-[1.6rem]">a</AvatarFallback>
           </Avatar>
-          <form className="relative w-full flex flex-col lg:gap-x-3 pt-7">
+          <form onSubmit={handleSumbit} className="relative w-full flex flex-col lg:gap-x-3 pt-7">
             <label htmlFor="Email">{t("name")}:</label>
-            <input onChange={(e) => e.target.value} className="outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type="email" />
+            <input onChange={handleChange} name="email" value={editprofile.email} className="outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type="email" />
             <div className="relative mt-5">
               <label htmlFor="Password">{t("password")}:</label>
-              <input onChange={(e) => e.target.value} className="relative w-full outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type={showPassword ? "text" : "password"} />
+              <input onChange={handleChange} name="password" value={editprofile.password} className="relative w-full outline-none border-1 border-zinc-500 px-3 py-1 mt-1 rounded-sm" type={showPassword ? "text" : "password"} />
               <button onClick={() => setShowPassword(!showPassword)} className="absolute right-3 translate-y-1/2" type="button">{showPassword ? <Unlock size={18} /> : <Lock size={18} />}</button>
             </div>
             <div className="flex items-center gap-x-3 mt-5">
