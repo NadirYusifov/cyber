0fc52297e16c7aab1a058b8fd51a388ba20d3cad
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-10 23:43:03 +0400

    profile data api added

--- frontend/app/(admin)/admin/home/layout.tsx
+++ frontend/app/(admin)/admin/home/layout.tsx
@@ -1,4 +1,3 @@
-import "@/app/globals.css";
 import Sidebar from "./sidebar/sidebar";
 
 // import {onCLS} from "web-vitals"
@@ -9,9 +8,17 @@ export default async function RootLayout({
   children: React.ReactNode,
 }>) {
 
+  const res = await fetch("http://localhost:7575/auth/profile", {
+    cache: "no-store"
+  })
+  const auth = await res.json()
+  if (!res.ok) {
+    throw new Error("Have a problem with the server.")
+  }
+
   return (
     <main className="admin-layout-main flex">
-      <Sidebar />
+      <Sidebar user={auth.user} />
       {children}
     </main>
   );
