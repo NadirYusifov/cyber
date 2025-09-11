d304c7e9a5b1275311eb56b455714fba320a80fb
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-11 18:53:44 +0400

    add product in input for file added but not completed

--- frontend/app/(admin)/admin/home/addproduct/page.tsx
+++ frontend/app/(admin)/admin/home/addproduct/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
 
 export default function AddProduct() {
   return (
-    <div>AddProduct</div>
+    <section className="w-full">
+      <div className="container mx-auto px-4">
+        <article className="mb-10">
+          <h3 className="text-[3rem]">Add product</h3>
+        </article>
+        <div>
+          <Input className="w-[31.25rem] h-72" type="file" />
+        </div>
+      </div>
+    </section>
   )
 }
