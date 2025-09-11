a26cfc507c0c00e809346e5de583ac19fb8cf9e0
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-09-11 18:54:56 +0400

    AdminValueCard added

---
+++ frontend/app/(admin)/admin/home/dashboard/@sectioncard/page.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AdminValueCard() {
+    return (
+        <section className="w-full mt-5">
+            <div className="container mx-auto px-4">
+                <article className="mb-10">
+                    <h3 className="text-[3rem] font-medium">Dashboard</h3>
+                </article>
+                <div className="flex items-center justify-center gap-4">
+                    <Card className="w-56 text-center">
+                        <CardHeader>Total Value</CardHeader>
+                        <CardTitle>$0</CardTitle>
+                    </Card>
+                    <Card className="w-56 text-center">
+                        <CardHeader>Total Value</CardHeader>
+                        <CardTitle>$0</CardTitle>
+                    </Card>
+                    <Card className="w-56 text-center">
+                        <CardHeader>Total Value</CardHeader>
+                        <CardTitle>$0</CardTitle>
+                    </Card>
+                </div>
+            </div>
+        </section>
+    )
+}
